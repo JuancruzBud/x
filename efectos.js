94dd55a2c9dd1c7ed403cd007024482ede632ff0
Juan Cruz Budin <juancruz.budin@gmail.com>
2017-05-23 00:55:44 -0300

    A

--- efectos.js
+++ efectos.js
@@ -11,6 +11,18 @@ $('#what-i-do-btn').click(function (event) {
   jQuery('html, body').animate({ scrollTop: Position }, 1100);
   return false;
 });
+$('#projects-btn').click(function (event) {
+  event.stopPropagation();
+  var Position = jQuery('[id="projects"]').offset().top;
+  jQuery('html, body').animate({ scrollTop: Position }, 1100);
+  return false;
+});
+$('#co-btn').click(function (event) {
+  event.stopPropagation();
+  var Position = jQuery('[id="contact"]').offset().top;
+  jQuery('html, body').animate({ scrollTop: Position }, 1100);
+  return false;
+});
 $('#scroll-down-btn').click(function (event) {
   event.stopPropagation();
   var Position = jQuery('[id="about"]').offset().top;
@@ -20,32 +32,55 @@ $('#scroll-down-btn').click(function (event) {
 
 // Hover en Box What i do
 document.getElementById("box-id").addEventListener("mouseover", function() {
-  document.getElementById("what-i-do").style.backgroundImage = "url(img/id-bg.png)";
+  document.getElementById("what-i-do").style.background = "url(img/ci-bg.jpg)";
 }, false);
 document.getElementById("box-id").addEventListener("mouseout", function() {
-   document.getElementById("what-i-do").style.backgroundImage = "";
+   document.getElementById("what-i-do").style.background = "";
 }, false);
 
-
-document.getElementById("box-ui").addEventListener("mouseover", function() {
-  document.getElementById("what-i-do").style.backgroundImage = "url(img/ui-bg.png)";
-}, false);
-document.getElementById("box-ui").addEventListener("mouseout", function() {
-   document.getElementById("what-i-do").style.backgroundImage = "";
-}, false);
+// Active en li Items
+$('nav li a').on('click', function(){
+    $('li a.activo').removeClass('activo');
+    $(this).addClass('activo');
+});
 
 
-document.getElementById("box-wd").addEventListener("mouseover", function() {
-  document.getElementById("what-i-do").style.backgroundImage = "url(img/wd-bg.png)";
-}, false);
-document.getElementById("box-wd").addEventListener("mouseout", function() {
-   document.getElementById("what-i-do").style.backgroundImage = "";
-}, false);
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
 
+    //smoothscroll
+    $('nav li a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
 
-document.getElementById("box-ci").addEventListener("mouseover", function() {
-  document.getElementById("what-i-do").style.backgroundImage = "url(img/ci-bg.png)";
-}, false);
-document.getElementById("box-ci").addEventListener("mouseout", function() {
-   document.getElementById("what-i-do").style.backgroundImage = "";
-}, false);
+        $('.navList li a').each(function () {
+            $(this).removeClass('activo');
+        })
+        $(this).addClass('activo');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#nav-menu li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#nav-menu ul li a').removeClass("activo");
+            currLink.addClass("activo");
+        }
+        else{
+            currLink.removeClass("activo");
+        }
+    });
+}
